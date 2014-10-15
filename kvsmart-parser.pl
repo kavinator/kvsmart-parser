0d04c0fd972d05934ce223fb8401d3af622cf024
Vladimir Petukhov <kavinator@gmail.com>
2014-10-15 11:08:20 +0400

    Allman style
    Signed-off-by: Vladimir Petukhov <kavinator@gmail.com>

--- kvsmart-parser.pl
+++ kvsmart-parser.pl
@@ -48,7 +48,8 @@ GetOptions(
     'help|h'             => \$HELP,
 ) or die "Incorrect usage!\n";
 
-sub print_usage() {
+sub print_usage()
+{
     print "$COPYRIGHT
 Usage: $0 [ OPTIONS ] [ VENDORS ] [ LOGPATH ] ... -drv='DRIVES'
 
@@ -100,20 +101,23 @@ either version 3 of the License, or (at your option) any later version.
 }
 
 # error_print( $error_message, $error_type )
-sub error_print {
+sub error_print
+{
     my $msg  = shift;
     my $type = shift || 'error';
     $type =~ s/(.*)/\U$1/g;
     print "$type: $msg\n";
 }
 
-sub debug_print {
+sub debug_print
+{
     print "$_[0]\n" if $DEBUG;
 }
 
 # file_read( $file_name )
 # @return: ref to array
-sub file_read {
+sub file_read
+{
     my $file_name = shift;
     my $output = [];
     open my $IN, '<', $file_name
@@ -124,22 +128,29 @@ sub file_read {
 }
 
 # log_write( $file_name, @array )
-sub log_write {
+sub log_write
+{
     my $file_name = shift;
     my $log_data  = shift;
     my $file_path = ( $file_name =~ /^(.*\/).*?$/ ) ? $1 : './';
     mkpath( $file_path, { error => \my $errmsg } );
-    if ( @$errmsg ) {
-        for my $diag ( @$errmsg ) {
+    if ( @$errmsg )
+    {
+        for my $diag ( @$errmsg )
+        {
             my ( $file, $message ) = %$diag;
-            if ( $file eq '' ) {
+            if ( $file eq '' )
+            {
                 &error_print( "general error: $message" );
-            } else {
+            }
+            else
+            {
                 &error_print( "create $file: $message" );
             }
         }
     }
-    if ( -e $file_name ) {
+    if ( -e $file_name )
+    {
         &debug_print( "file \"$file_name\" exist, replaced" );
         unlink $file_name;
     }
@@ -151,23 +162,30 @@ sub log_write {
 }
 
 # split_names( @names )
-sub split_names {
+sub split_names
+{
     return grep{ defined } split( /[,\ ]\s*/, join( ',', @_ ) )
 }
 
 # drives_check( @drives )
 # @return ref to array
-sub drives_check {
+sub drives_check
+{
     my $drives = shift;
-    unless ( $drives ) {
+    unless ( $drives )
+    {
         return [];
     }
     my $rigth_drives = [];
-    for ( &split_names( @$drives ) ) {
-        if ( m{^\s*(/dev/.+)\s*?$} and -e $1 ) {
+    for ( &split_names( @$drives ) )
+    {
+        if ( m{^\s*(/dev/.+)\s*?$} and -e $1 )
+        {
             &debug_print( "\"$1\" exist" );
             push @$rigth_drives, $1;
-        } else {
+        }
+        else
+        {
             &error_print(
                 "drive \"$1\" not exist",
                 "warning"
@@ -180,19 +198,25 @@ sub drives_check {
 
 # vendor_check( @drives )
 # @return: ref to array
-sub vendor_check {
+sub vendor_check
+{
     my $drives  = shift;
     my $vendors = shift;
-    unless ( @$vendors ) {
+    unless ( @$vendors )
+    {
         return @$drives;
-    } else {
+    }
+    else
+    {
         @$vendors = &split_names( @$vendors );
         &debug_print( "Detected vendors: " . join( ', ', @$vendors ) );
         my $right_drives = [];
-        for my $drive ( @$drives ) {
+        for my $drive ( @$drives )
+        {
             $drive =~ m{/dev/(\w+)};
             my $model_path = "/sys/block/$1/device/model";
-            if ( -r "$model_path" ) {
+            if ( -r "$model_path" )
+            {
                 my $vendor = ( split /\s+/, @{ &file_read( $model_path ) }[0] )[0];
                 &debug_print( "drive \"$drive\" vendor \"$vendor\"" );
                 push @$right_drives, $drive
@@ -204,9 +228,11 @@ sub vendor_check {
 }
 
 # smart_attr_check( @attributes )
-sub smart_attr_check {
+sub smart_attr_check
+{
     my $attributes = shift;
-    if ( @$attributes ) {
+    if ( @$attributes )
+    {
         $attributes = [ &split_names( @$attributes ) ];
         &debug_print( "Detected SMART attributes: " . join( ', ', @$attributes ) );
     }
@@ -232,54 +258,67 @@ sub smart_attr_check {
 #
 # run_smart( $drive_name )
 # @return: ref to hash
-sub run_smart {
+sub run_smart
+{
     my $drive = shift;
     my $cmd = "$SMARTCTL --attributes $drive --format=$FORMAT";
     &debug_print( "run smartctl for $drive" );
     my $smart_result = [ `$cmd` ];
     my $found_start_tag;
     my $errmsg = "";
-    for ( @$smart_result ) {
+    for ( @$smart_result )
+    {
         chomp;
-        if ( /^ID#\s+ATTRIBUTE_NAME\s+FLAG/ ) {
+        if ( /^ID#\s+ATTRIBUTE_NAME\s+FLAG/ )
+        {
             $found_start_tag = 1;
             last;
         }
-        if ( /Permission denied/ ) {
+        if ( /Permission denied/ )
+        {
             $errmsg = $_;
             last;
         }
-        if ( /command not found/ ) {
+        if ( /command not found/ )
+        {
             $errmsg = "command error: $_";
             last;
         }
-        if ( /open device: .* failed/ ) {
+        if ( /open device: .* failed/ )
+        {
             $errmsg = $_;
             last;
         }
-        if ( /\[this device: CD\/DVD\]/ ) {
+        if ( /\[this device: CD\/DVD\]/ )
+        {
             $errmsg = "device is a CD/DVD drive.";
             last;
         }
-        if ( /^Smartctl: Device Read Identity Failed \(not an ATA\/ATAPI device\)/ ) {
+        if ( /^Smartctl: Device Read Identity Failed \(not an ATA\/ATAPI device\)/ )
+        {
             $errmsg = "device does not exist.";
             last;
         }
-        if ( /Unable to detect device type/ ) {
+        if ( /Unable to detect device type/ )
+        {
             $errmsg = $_;
             last;
         }
     }
-    if ( $errmsg eq "" && !$found_start_tag ) {
+    if ( $errmsg eq "" && !$found_start_tag )
+    {
         $errmsg = "parse error, no param start tag found!";
     }
-    if ( $errmsg ) {
+    if ( $errmsg )
+    {
         &error_print( $errmsg );
         exit;
     }
     my %smart_data;
-    for ( @$smart_result ) {
-        if ( /^\s*(?:\d{1,3}|ID#)\s+.*?$/ ) {
+    for ( @$smart_result )
+    {
+        if ( /^\s*(?:\d{1,3}|ID#)\s+.*?$/ )
+        {
             chomp;
 
             # Example of smartctl output for ATA-drive (old-format)
@@ -317,7 +356,8 @@ sub run_smart {
                 \s*$
             )x;
 
-            unless ( $+{ attr_name } ~~ %smart_data ) {
+            unless ( $+{ attr_name } ~~ %smart_data )
+            {
                 $smart_data{ $+{ attr_name } } = {
                     id        => $+{ id },
                     flag      => $+{ flag },
@@ -327,8 +367,10 @@ sub run_smart {
                     fail      => $+{ fail },
                     raw_value => $+{ raw_value },
                 };
-                if ( $+{ old_format } ) {
-                    for my $section ( qw( type updated ) ) {
+                if ( $+{ old_format } )
+                {
+                    for my $section ( qw( type updated ) )
+                    {
                         $smart_data{ $+{ attr_name } }{ $section } = $+{ $section }
                             if $+{ $section };
                     }
@@ -343,27 +385,33 @@ sub run_smart {
 # Main part
 ########################################################################
 
-if ( $HELP or $#ARGV == 0 ) {
+if ( $HELP or $#ARGV == 0 )
+{
     &print_usage();
     exit;
-} elsif ( $VER ) {
+}
+elsif ( $VER )
+{
     print "$COPYRIGHT\n";
     exit;
 }
 
-if ( $ENV{ USER } ne 'root' ) {
+if ( $ENV{ USER } ne 'root' )
+{
     &error_print(
         'root privileges are required to detect vendor or run smartctl!',
         'warning'
     );
 }
 
-unless ( -x $SMARTCTL ) {
+unless ( -x $SMARTCTL )
+{
     print "\nERROR: cannot find smartctl\n\n";
     exit;
 }
 
-unless ( $FORMAT eq 'old' or $FORMAT eq 'brief' ) {
+unless ( $FORMAT eq 'old' or $FORMAT eq 'brief' )
+{
     &error_print( "invalid smart output format: $FORMAT" );
     exit;
 }
@@ -378,27 +426,34 @@ unless ( $FORMAT eq 'old' or $FORMAT eq 'brief' ) {
 @ATTRIBUTES = &smart_attr_check( \@ATTRIBUTES )
     if @ATTRIBUTES;
 
-for my $drive ( @DRIVES ) {
+for my $drive ( @DRIVES )
+{
     &debug_print( "use $drive" );
     my $drive_smart = &run_smart( $drive );
-    if ( %$drive_smart ) {
+    if ( %$drive_smart )
+    {
         $drive =~ m{/dev/(\w+)};
         my @smart_log  = ();
         my $attributes = [ keys %$drive_smart ];
-        if ( @ATTRIBUTES ) {
+        if ( @ATTRIBUTES )
+        {
             @$attributes = grep{ $_ if $_ ~~ @ATTRIBUTES } @$attributes;
             push @$attributes, 'ATTRIBUTE_NAME';
         }
-        for my $attr ( sort @$attributes ) {
+        for my $attr ( sort @$attributes )
+        {
             my %attr_data = %{ $drive_smart->{ $attr } };
             # order of smart-data colums
             my $columns = [ qw( id flag value worst thresh type updated fail raw_value ) ];
             my $values  = [ grep{ defined } @attr_data{ @$columns } ];
             push @smart_log, join( $SEP_OUTPUT, $drive, $attr, @$values ) . "\n";
         }
-        if ( $LOG_PATH ) {
+        if ( $LOG_PATH )
+        {
             &log_write( "$LOG_PATH/$1.log", \@smart_log );
-        } else {
+        }
+        else
+        {
             print @smart_log;
         }
     }
